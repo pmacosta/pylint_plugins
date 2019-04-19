0ca2fbdcb87f19bf081839adcae8eb040c80fec1
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-04-19 09:17:06 -0400

    Failsafe to hunspell via shell if CyHunspell does not work

--- common.py
+++ common.py
@@ -1,12 +1,22 @@
 # common.py
 # Copyright (c) 2018-2019 Pablo Acosta-Serafini
 # See LICENSE for details
-# pylint: disable=C0111,R0205,R0912,W0611
+# pylint: disable=C0111,E1129,R0205,R0912,W0611,W1113
 
 # Standard library imports
 from __future__ import print_function
+import collections
 import os
+import platform
+import re
+import subprocess
 import sys
+import tempfile
+import time
+import types
+
+# PyPI imports
+import decorator
 
 # Literal copy from [...]/site-packages/pip/_vendor/compat.py
 try:
@@ -94,11 +104,58 @@ def _find_ref_fname(fname, ref_fname):
     return ""
 
 
+def _grep(fname, words):
+    """Return line numbers in which words appear in a file."""
+    # pylint: disable=W0631
+    pat = "(.*[^a-zA-Z]|^){}([^a-zA-Z].*|$)"
+    regexps = [(word, re.compile(pat.format(word))) for word in words]
+    ldict = collections.defaultdict(list)
+    for num, line in enumerate(_read_file(fname)):
+        for word in [word for word, regexp in regexps if regexp.match(line)]:
+            ldict[word].append(num + 1)
+    return ldict
+
+
+def _make_abspath(value):
+    """Homogenize files to have absolute paths."""
+    value = value.strip()
+    if not os.path.isabs(value):
+        value = os.path.abspath(os.path.join(os.getcwd(), value))
+    return value
+
+
 def _read_file(fname):
     """Return file lines as strings."""
     with open(fname) as fobj:
         for line in fobj:
             yield _tostr(line).strip()
+
+
+def _shcmd(cmd, timeout=15):
+    """Safely execute shell command."""
+    delay = 1.0
+    obj = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if sys.hexversion < 0x03000000:
+        while (obj.poll() is None) and (timeout > 0):
+            time.sleep(delay)
+            timeout -= delay
+        if not timeout:
+            obj.kill()
+        stdout, stderr = obj.communicate()
+    else:
+        try:
+            stdout, stderr = obj.communicate(timeout=timeout)
+        except subprocess.TimeoutExpired:
+            obj.kill()
+            stdout, stderr = obj.communicate()
+    if obj.returncode:
+        print("COMMAND: " + (" ".join(cmd)))
+        print("STDOUT:" + os.linesep + _tostr(stdout))
+        print("STDERR:" + os.linesep + _tostr(stderr))
+        raise RuntimeError("Shell command could not be executed successfully")
+    stdout = _tostr(stdout).split(os.linesep)
+    stderr = _tostr(stderr).split(os.linesep)
+    return stdout, stderr
 
 
 def _tostr(obj):  # pragma: no cover
@@ -119,4 +176,51 @@ class StreamFile(object):
                 yield line
 
     def __exit__(self, exc_type, exc_value, exc_tb):  # noqa
+        return not exc_type is not None
+
+
+@decorator.contextmanager
+def ignored(*exceptions):
+    try:
+        yield
+    except exceptions:
+        pass
+
+
+class TmpFile(object):
+    """
+    Use a temporary file within context.
+
+    From pmisc package
+    """
+
+    def __init__(self, fpointer=None, *args, **kwargs):  # noqa
+        if (
+            fpointer
+            and (not isinstance(fpointer, types.FunctionType))
+            and (not isinstance(fpointer, types.LambdaType))
+        ):
+            raise RuntimeError("Argument `fpointer` is not valid")
+        self._fname = None
+        self._fpointer = fpointer
+        self._args = args
+        self._kwargs = kwargs
+
+    def __enter__(self):  # noqa
+        fdesc, fname = tempfile.mkstemp()
+        # fdesc is an OS-level file descriptor, see problems if this
+        # is not properly closed in this post:
+        # https://www.logilab.org/blogentry/17873
+        os.close(fdesc)
+        if platform.system().lower() == "windows":  # pragma: no cover
+            fname = fname.replace(os.sep, "/")
+        self._fname = fname
+        if self._fpointer:
+            with open(self._fname, "w") as fobj:
+                self._fpointer(fobj, *self._args, **self._kwargs)
+        return self._fname
+
+    def __exit__(self, exc_type, exc_value, exc_tb):  # noqa
+        with ignored(OSError):
+            os.remove(self._fname)
         return not exc_type is not None
--- spellcheck.py
+++ spellcheck.py
@@ -1,7 +1,8 @@
 # spellcheck.py
 # Copyright (c) 2018-2019 Pablo Acosta-Serafini
 # See LICENSE for details
-# pylint: disable=C0111,C0325,C0411,E1101,E1123,E1129,R0205,R0903,R0912,R1718,W0611,W1113
+# pylint: disable=C0111,C0325,C0411,E1101,E1123,E1129
+# pylint: disable=R0205,R0903,R0912,R0914,R1718,W0611,W0702,W1113
 
 # Standard library imports
 from fnmatch import fnmatch
@@ -11,12 +12,27 @@ import re
 import sys
 
 # PyPI imports
-import hunspell
+try:
+    import hunspell
+
+    NATIVE = True
+except:
+    NATIVE = False
 from pylint.interfaces import IRawChecker
 from pylint.checkers import BaseChecker
 
 # Intra-package imports
-from common import _find_ref_fname, _read_file, _tostr, which, StreamFile
+from common import (
+    _find_ref_fname,
+    _grep,
+    _make_abspath,
+    _read_file,
+    _shcmd,
+    _tostr,
+    which,
+    StreamFile,
+    TmpFile,
+)
 
 
 ###
@@ -29,52 +45,83 @@ REF_EXCLUDE = os.path.join("data", "exclude-spelling")
 ###
 # Functions
 ###
-def _make_abspath(value):
-    """Homogenize files to have absolute paths."""
-    value = value.strip()
-    if not os.path.isabs(value):
-        value = os.path.abspath(os.path.join(os.getcwd(), value))
-    return value
-
-
 def check_spelling(fname, whitelist_fname="", exclude_fname=""):
     """Check spelling against whitelist."""
-    # pylint: disable=R0914
     fname = os.path.abspath(fname)
     whitelist_fname = whitelist_fname.strip() or _find_ref_fname(fname, REF_WHITELIST)
     exclude_fname = exclude_fname.strip() or _find_ref_fname(fname, REF_EXCLUDE)
-    if whitelist_fname:
-        whitelist_fname = os.path.abspath(whitelist_fname)
-        whitelist = []
-        if not os.path.exists(whitelist_fname):
-            print("WARNING: Whitelist file {0} not found".format(whitelist_fname))
-        else:
-            with open(whitelist_fname, "r") as fobj:
-                whitelist = [item.strip() for item in fobj]
-            # print("Using {0}".format(whitelist_fname))
-    if exclude_fname:
-        exclude_fname = os.path.abspath(exclude_fname)
-        if not os.path.exists(exclude_fname):
-            print("WARNING: exclude file {0} not found".format(exclude_fname))
-        # print("Using {0}".format(exclude_fname))
-    if os.path.exists(exclude_fname):
-        patterns = [_make_abspath(item) for item in _read_file(exclude_fname)]
-        if any(fnmatch(fname, pattern) for pattern in patterns):
-            return []
-    spell_obj = hunspell.Hunspell("en_US")
-    ret = []
-    with open(fname, "r") as fobj:
-        for num, line in enumerate(fobj):
-            line = line.strip()
-            for word in re.split("[^a-zA-Z]", line):
-                if (not spell_obj.spell(word)) and (word not in whitelist):
-                    ret.append((num + 1, (word,)))
-    return ret
+    obj = Hunspell(NATIVE, whitelist_fname, exclude_fname)
+    return obj.check(fname)
 
 
 ###
 # Classes
 ###
+class Hunspell(object):
+    """
+    Alternate implementation of CyHunspell.
+
+    Useful when incompatibilities between libhunspell requirements of CyHunspell and
+    the installed hunspell package.
+    """
+
+    def __init__(self, native, whitelist_fname, exclude_fname):
+        """Check that hunspell binary can be found."""
+        if not which("hunspell"):
+            print("hunspell binary not found, skipping")
+        self.native = native
+        self.cmd = ["hunspell"]
+        self.whitelist = []
+        if whitelist_fname:
+            whitelist_fname = os.path.abspath(whitelist_fname)
+            if not os.path.exists(whitelist_fname):
+                print("WARNING: Whitelist file {0} not found".format(whitelist_fname))
+            else:
+                self.cmd += ["-p", whitelist_fname]
+                with open(whitelist_fname, "r") as fobj:
+                    self.whitelist = [item.strip() for item in fobj]
+        self.cmd += ["-l"]
+        self.exclude_fname = None
+        if exclude_fname:
+            exclude_fname = os.path.abspath(exclude_fname)
+            if not os.path.exists(exclude_fname):
+                print("WARNING: exclude file {0} not found".format(exclude_fname))
+            self.exclude_fname = exclude_fname
+
+    def check(self, fname):
+        """Check file."""
+        if os.path.exists(self.exclude_fname):
+            patterns = [_make_abspath(item) for item in _read_file(self.exclude_fname)]
+            if any(fnmatch(fname, pattern) for pattern in patterns):
+                return []
+        ret = []
+        if not self.native:
+            # hunspell has trouble with apostrophes and other delimiters out-of-the-box
+            words = []
+            with open(fname, "r") as fobj:
+                for line in fobj:
+                    for word in re.split("[^a-zA-Z]", line.strip()):
+                        words.append(word)
+            with TmpFile(lambda x: x.write(os.linesep.join(words))) as temp_fname:
+                stdout, _ = _shcmd(self.cmd + [temp_fname])
+            words = sorted(list(set([word.strip() for word in stdout if word.strip()])))
+            if words:
+                ldict = _grep(fname, words)
+                for word, lines in [(word, ldict[word]) for word in words]:
+                    for lnum in lines:
+                        ret.append((lnum, (word,)))
+        else:
+            spell_obj = hunspell.Hunspell("en_US")
+            ret = []
+            with open(fname, "r") as fobj:
+                for num, line in enumerate(fobj):
+                    line = line.strip()
+                    for word in re.split("[^a-zA-Z]", line):
+                        if (not spell_obj.spell(word)) and (word not in self.whitelist):
+                            ret.append((num + 1, (word,)))
+        return ret
+
+
 class SpellChecker(BaseChecker):
     """Check for spelling."""
 
